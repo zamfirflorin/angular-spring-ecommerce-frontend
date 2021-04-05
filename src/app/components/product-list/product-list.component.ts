0d55d0b755a3a59308fbfca4b19c7bbbe31b1d8c
fszamfi <zamfirflorinstelian@yahoo.com>
2021-04-05 22:19:24 +0300

    added pagination

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -18,7 +18,7 @@ export class ProductListComponent implements OnInit {
         //new properties for pagination
 
         thePageNumber: number = 1;
-        thePageSize: number = 10;
+        thePageSize: number = 50;
         theTotalElements: number = 0; 
         
 
