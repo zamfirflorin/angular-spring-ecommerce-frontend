d738517278153c7da48e0f845c4ba71e213adc35
fszamfi <zamfirflorinstelian@yahoo.com>
2021-04-06 22:19:29 +0300

    added employee table

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -4,34 +4,34 @@ import { Product } from 'src/app/common/product';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-	selector: 'app-product-list',
-	templateUrl: './product-list-grid.component.html',
-	styleUrls: ['./product-list.component.css']
+        selector: 'app-product-list',
+        templateUrl: './product-list-grid.component.html',
+        styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
         products: Product[] = [];
         currentCategoryId: number = 1;
-        previousCategoryId: number = 1; 
+        previousCategoryId: number = 1;
         searchMode: boolean = false;
 
         //new properties for pagination
 
         thePageNumber: number = 1;
-        thePageSize: number = 50;
-        theTotalElements: number = 0; 
-        
+        thePageSize: number = 5;
+        theTotalElements: number = 0;
 
-	constructor(private productService: ProductService,
-		                private route: ActivatedRoute) { }
 
-	ngOnInit() {
-		this.route.paramMap.subscribe(() => {
-			this.listProducts();
-		});
-	}
+        constructor(private productService: ProductService,
+                private route: ActivatedRoute) { }
 
-	listProducts() {
+        ngOnInit() {
+                this.route.paramMap.subscribe(() => {
+                        this.listProducts();
+                });
+        }
+
+        listProducts() {
 
                 //check to see if this route has a parameter for this keyword because if it does have a keyword for the parameter this means we are performing a search
                 this.searchMode = this.route.snapshot.paramMap.has('keyword');
@@ -51,7 +51,7 @@ export class ProductListComponent implements OnInit {
 
                 this.productService.searchProducts(theKeyword).subscribe(
                         data => {
-                                this.products = data; 
+                                this.products = data;
                         }
                 )
         }
@@ -60,22 +60,19 @@ export class ProductListComponent implements OnInit {
         handleListProducts() {
 
                 const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
-		if (hasCategoryId) {
-			this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
-		} else {
-			this.currentCategoryId = 1;
-		}
-
+                if (hasCategoryId) {
+                        this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+                } else {
+                        this.currentCategoryId = 1;
+                }
                 //check if we have a different category than previous
                 //Note: Angular will reuse a component if it is currently being viewed
                 //if we have a different category id than previous then we want to reset the page number back to 1
 
                 if (this.previousCategoryId != this.currentCategoryId) {
-                        this.thePageNumber = 1; 
+                        this.thePageNumber = 1;
                 }
-
                 this.previousCategoryId = this.currentCategoryId;
-
                 console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
 
                 this.productService.getProductListPaginate(this.thePageNumber - 1,
@@ -87,7 +84,13 @@ export class ProductListComponent implements OnInit {
                         this.products = data._embedded.products;
                         this.thePageNumber = data.page.number + 1;
                         this.thePageSize = data.page.size;
-                        this.theTotalElements = data.page.totalElements; 
+                        this.theTotalElements = data.page.totalElements;
                 }
+        }
+
+        updatePageSize(pageSize: number) {
+                this.thePageSize = pageSize;
+                this.thePageNumber = 1;
+                this.listProducts();
         }
 }
